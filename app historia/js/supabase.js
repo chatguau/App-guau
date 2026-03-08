5352b7dd6f0f00d88a6e63f7be8af8f6aa0bb3ce
chatguau <chatguau@gmail.com>
2026-03-08 13:56:57 -0500

    Update supabase.js

--- app historia/js/supabase.js
+++ app historia/js/supabase.js
@@ -4,7 +4,7 @@
  */
 
 const SUPABASE_URL = 'https://liriysxowbwqrgpygjkl.supabase.co';
-const SUPABASE_ANON_KEY = '<TU_SUPABASE_ANON_KEY>'; // <-- PEGA TU CLAVE AQUÍ
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxpcml5c3hvd2J3cXJncHlnamtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5ODc0ODEsImV4cCI6MjA4ODU2MzQ4MX0.-zvts06ylfB9XUzWuYbh45eMLv-uJI2SHUYzDlCec3Q'; // <-- PEGA TU CLAVE AQUÍ
 
 // Inicializar el cliente de Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
